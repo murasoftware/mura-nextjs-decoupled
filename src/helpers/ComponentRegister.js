cfc73ee62050622f667361d15e8b4b7b1e1cd64c
Matt Levine <matt.levine@blueriver.com>
2020-06-30 10:39:54 -0700

    Adding example module component

--- src/helpers/ComponentRegister.js
+++ src/helpers/ComponentRegister.js
@@ -1,19 +1,25 @@
 import Image from '../components/Image';
 import Container from '../components/Container';
+import Example from '../components/Example';
 import Video from '../components/Video';
 import Text from '../components/Text';
+import Mura from 'mura.js';
 
 const getComponent = (item,setStyleRegions) => {
   let Component;
+  const objectkey=Mura.firstToUpperCase(item.object);
 
 //  console.log("getComponent -> item.objectname: ",setStyleRegions);
 
-  switch (item.objectname) {
+  switch (objectkey) {
     case 'Image':
       Component = <Image key={item.instanceid} {...item} />;
       break;
     case 'Container':
       Component = <Container key={item.instanceid} {...item} setStyleRegions={setStyleRegions}/>;
+      break;
+    case 'Example':
+      Component = <Example key={item.instanceid} {...item} setStyleRegions={setStyleRegions}/>;
       break;
     case '':
       if(item.sourcetype == 'component') {
