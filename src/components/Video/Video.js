41f192dbb5ab33979bd34abf3d1752881f468e2d
Grant Shepert <github@sodaseven.com>
2020-07-09 12:07:54 +0100

    Video updates

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,16 +1,48 @@
 import MuraDecorator from '../MuraDecorator';
-import Youtube from './Youtube'
-
+import Youtube from './Youtube';
+import Vimeo from './Vimeo';
+import Vidyard from './Vidyard';
+import Wistia from './Wistia';
+import ModalVideo from './ModalVideo';
+import {videoConfig} from './videoConfig';
 
 const getVideoPlayer = props => {
   const players = {};
   players['youtube'] = Youtube(props);
+  players['vimeo'] = Vimeo(props);
+  players['wistia'] = Wistia(props);
+  players['vidyard'] = Vidyard(props);
+
   return players[props.videoplatform];
 };
 
 function Video(props) {
-  const { instanceid, videoid, videoplatform } = props;
-  return getVideoPlayer(props);
+
+console.log("VIDEO",props.displaytype);
+
+  const { displaytype, videoid, videoplatform } = props;
+  const {modalsize} = videoConfig;
+
+  console.log("MSIZE",modalsize);
+
+  if(!videoplatform.length) {
+    return <div>Video platform missing.</div>
+  }
+  else if(!videoid) {
+    return <div>Video id missing.</div>
+  }
+  else {
+    if(displaytype === 'modal') {
+      return (
+      <ModalVideo props={props}>
+        {getVideoPlayer(props)}
+      </ModalVideo>
+      )
+    }
+    else {
+      return getVideoPlayer(props);
+    }
+  }
 }
 
 export default Video;
