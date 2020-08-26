f416424a2c7990370dd540e17d3d94d031107b72
Matt Levine <matt.levine@blueriver.com>
2020-08-26 10:19:13 -0700

    Updating edit routes to not include static routes

--- src/pages/edit/index.js
+++ src/pages/edit/index.js
@@ -33,7 +33,7 @@ function Edit(props) {
 
   return (
     <EditLayout {...props}>
-      <MainLayout {...props}>
+      <MainLayout {...props} route={`/${router.query.page}`}>
         <Head>
           <link
             href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
